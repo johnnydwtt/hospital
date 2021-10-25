bb26e9b8ca5a419356a3c40955366fbac07e582e
johnnydwtt <johnnydwtt@gmail.com>
2021-10-25 13:12:26 +0200

    ajout Patient.php Class

--- models/Patient.php
+++ models/Patient.php
@@ -4,6 +4,7 @@
 require_once(dirname(__FILE__).'/../config/connect.php');
 
 class Patient{
+
     private $_lastname;
     private $_firstname;
     private $_birthdate;
@@ -19,26 +20,25 @@ class Patient{
         $this->_mail = $mail;
     }
 
-}
-    
-
-    //on écris la requête
-    $sql = 'INSERT INTO `patients`(`lastname`,`firstname`,`birthdate`,`phone`,`mail	`) VALUES (:lastname,:firstname,:birthdate,:phone,:mail)';
-    //on prépare la requête
-    $query = $pdo->prepare($sql);
-    //on injecte les valeurs
-    $query->bindvalue(":lastname", $lastname, PDO::PARAM_STR);
-    $query->bindvalue(":firstname", $firstname, PDO::PARAM_STR);
-    $query->bindvalue(":phone", $phone, PDO::PARAM_STR);
-    $query->bindvalue(":mail", $mail, PDO::PARAM_STR);
-    $query->bindvalue(":birthdate", $birthdate, PDO::PARAM_STR);
-
-    //on exécute la requête
-    if(!$query->execute()){
-        die("une erreur est survenue");
+    public function insertPatient(){
+        //on écris la requête
+        $sql = 'INSERT INTO `patients`(`lastname`,`firstname`,`birthdate`,`phone`,`mail	`) VALUES (:lastname,:firstname,:birthdate,:phone,:mail)';
+        //on prépare la requête
+        $query = $pdo->prepare($sql);
+        //on injecte les valeurs
+        $query->bindvalue(":lastname", $lastname, PDO::PARAM_STR);
+        $query->bindvalue(":firstname", $firstname, PDO::PARAM_STR);
+        $query->bindvalue(":phone", $phone, PDO::PARAM_STR);
+        $query->bindvalue(":mail", $mail, PDO::PARAM_STR);
+        $query->bindvalue(":birthdate", $birthdate, PDO::PARAM_STR);
+        
+        //on exécute la requête
+        if(!$query->execute()){
+            die("une erreur est survenue");
+        }
     }
+}
 
-    //on récupére l'id du patient
-    $id = $pdo->lastInsertId();
-    die("patient ajouté avec l'id $id");
+
+
 
