052da9dc578b9985697eeac2f72e5e55947dee9e
johnnydwtt <johnnydwtt@gmail.com>
2021-10-27 11:22:40 +0200

    error 404

---
+++ 404.php
@@ -0,0 +1,14 @@
+<div id="clouds">
+            <div class="cloud x1"></div>
+            <div class="cloud x1_5"></div>
+            <div class="cloud x2"></div>
+            <div class="cloud x3"></div>
+            <div class="cloud x4"></div>
+            <div class="cloud x5"></div>
+        </div>
+        <div class="c">
+            <div class="_404">404</div>
+            <div class="_1">PAGE</div>
+            <div class="_2">NON TROUVE</div>
+            <a href="/controllers/welcomeCtrl.php"><button type="button" class="btn btn-primary">RETOURNER A L'ACCUEIL</button></a>
+</div>
---
+++ controllers/404Ctrl.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Erreur 404 - Hospital';
+
+include(dirname(__FILE__).'/../views/templates/header.php');
+include(dirname(__FILE__).'/../404.php');
+include(dirname(__FILE__).'/../views/templates/footer.php');
